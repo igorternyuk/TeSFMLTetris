33c9cf4502f6ea974f6c277445dad065e4566305
igorternyuk <mr.ternyuk@mail.ru>
2018-12-23 12:00:13 +0200

    drawing tetrominos

--- game.cpp
+++ game.cpp
@@ -1,10 +1,17 @@
 #include "game.hpp"
+#include <ctime>
+#include <iostream>
 
 Game::Game():
     mWindow(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT),
             TITLE_OF_MAIN_WINDOW, sf::Style::Close)
 {
-
+    createAssets();
+    initField();
+    srand(time(nullptr));
+    spawnNewPiece();
+    mWindow.setFramerateLimit(FPS);
+    centralizeWindow();
 }
 
 void Game::run()
@@ -31,16 +38,190 @@ void Game::inputPhase()
     {
         if(event.type == sf::Event::Closed)
             mWindow.close();
+        if(event.type == sf::Event::KeyReleased)
+        {
+            if(event.key.code == sf::Keyboard::N)
+            {
+                spawnNewPiece();
+            }
+        }
     }
 }
 
 void Game::updatePhase(sf::Time frameTime)
 {
 
+
 }
 
 void Game::renderPhase()
 {
-    mWindow.clear();
+    mWindow.clear(mColors[EMPTY]);
+    drawField();
+    drawCurrentPiece();
     mWindow.display();
+}
+
+void Game::drawField()
+{
+    for(int y = 0; y < FIELD_HEIGHT; ++y)
+    {
+        for(int x = 0; x < FIELD_WIDTH; ++x)
+        {
+            sf::RectangleShape block(sf::Vector2f(BLOCK_SIZE,BLOCK_SIZE));
+            block.setPosition(x * BLOCK_SIZE, y * BLOCK_SIZE);
+            block.setFillColor(mColors[mField[y * FIELD_WIDTH + x]]);
+            bool isBound = (x == 0 || x == FIELD_WIDTH - 1) || (y == FIELD_HEIGHT - 1);
+            if(!isBound)
+            {
+                block.setOutlineColor(sf::Color::Green);
+                block.setOutlineThickness(1);
+            }
+            mWindow.draw(block);
+        }
+    }
+
+}
+
+void Game::drawCurrentPiece()
+{
+    for(int py = 0; py < BLOCKS_IN_PIECE; ++py)
+    {
+        for(int px = 0; px < BLOCKS_IN_PIECE; ++px)
+        {
+            int pi = rotate(px, py, mCurrentPiece.rotation);
+            if(mShapes[mCurrentPiece.type][pi] == 'X')
+            {
+                int fx = mCurrentPiece.x + px;
+                int fy = mCurrentPiece.y + py;
+                sf::RectangleShape piece(sf::Vector2f(BLOCK_SIZE,BLOCK_SIZE));
+
+                piece.setPosition(fx * BLOCK_SIZE, fy * BLOCK_SIZE);
+                piece.setFillColor(mColors[mCurrentPiece.type]);
+                piece.setOutlineColor(sf::Color(70,70,70));
+                piece.setOutlineThickness(1);
+                mWindow.draw(piece);
+            }
+        }
+    }
+}
+
+bool Game::fit(const Game::Tetramino &tetramino, int destX, int destY, int rotation)
+{
+    for(int py = 0; py < BLOCKS_IN_PIECE; ++py)
+    {
+        for(int px = 0; px < BLOCKS_IN_PIECE; ++px)
+        {
+            int pi = rotate(px, py, rotation);
+            int fieldX = destX + px;
+            int fieldY = destY + py;
+            int fieldIndex = fieldY * FIELD_WIDTH + fieldX;
+            if(isValidCoordinates(fieldX, fieldY))
+            {
+                if(mField[fieldIndex] != EMPTY && mShapes[tetramino.type][pi] == 'X')
+                {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+int Game::rotate(int blockX, int blockY, int angle) const
+{
+    switch (angle % 4) {
+        default: return blockY * 4 + blockX;      // 0 degrees;
+        case 1: return 12 + blockY - 4 * blockX; // 90 degrees;
+        case 2: return 15 - blockY * 4 - blockX; // 180 degrees;
+        case 3: return 3 - blockY + 4 * blockX;  // 270 degrees;
+    }
+}
+
+void Game::initField()
+{
+    for(int y = 0; y < FIELD_HEIGHT; ++y)
+    {
+        for(int x = 0; x < FIELD_WIDTH; ++x)
+        {
+            int index = y * FIELD_WIDTH + x;
+            bool isBound = (x == 0 || x == FIELD_WIDTH - 1) || (y == FIELD_HEIGHT - 1);
+            mField[index] = isBound ? BOUNDARY_BLOCK : EMPTY;
+        }
+    }
+}
+
+void Game::spawnNewPiece()
+{
+    mCurrentPiece.x = FIELD_WIDTH / 2 - 2;
+    mCurrentPiece.y = 0;
+    mCurrentPiece.rotation = 0;
+    mCurrentPiece.type = rand() % NUM_SHAPES + 1;
+}
+
+void Game::createAssets()
+{
+    mColors[EMPTY] = sf::Color(0x807962ff);
+    mColors[BOUNDARY_BLOCK] = sf::Color(0xb7c2c6ff);
+    // I-shape
+    mShapes[SHAPE_I].append("..X.");
+    mShapes[SHAPE_I].append("..X.");
+    mShapes[SHAPE_I].append("..X.");
+    mShapes[SHAPE_I].append("..X.");
+    mColors[SHAPE_I] = sf::Color::Red;
+
+    // L-shape
+    mShapes[SHAPE_L].append(".X..");
+    mShapes[SHAPE_L].append(".X..");
+    mShapes[SHAPE_L].append(".XX.");
+    mShapes[SHAPE_L].append("....");
+    mColors[SHAPE_L] = sf::Color(0x81ae04ff);
+
+    // J-shape
+    mShapes[SHAPE_J].append("..X.");
+    mShapes[SHAPE_J].append("..X.");
+    mShapes[SHAPE_J].append(".XX.");
+    mShapes[SHAPE_J].append("....");
+    mColors[SHAPE_J] = sf::Color(0xf6546aff);
+
+    // O-shape
+    mShapes[SHAPE_O].append("....");
+    mShapes[SHAPE_O].append(".XX.");
+    mShapes[SHAPE_O].append(".XX.");
+    mShapes[SHAPE_O].append("....");
+    mColors[SHAPE_O] = sf::Color(0x4a6ca8ff);
+
+    // S-shape
+    mShapes[SHAPE_S].append("..X.");
+    mShapes[SHAPE_S].append(".XX.");
+    mShapes[SHAPE_S].append(".X..");
+    mShapes[SHAPE_S].append("....");
+    mColors[SHAPE_S] = sf::Color(0x420690ff);
+
+    // Z-shape
+    mShapes[SHAPE_Z].append(".X.");
+    mShapes[SHAPE_Z].append(".XX.");
+    mShapes[SHAPE_Z].append("..X.");
+    mShapes[SHAPE_Z].append("....");
+    mColors[SHAPE_Z] = sf::Color(0xffb90fff);
+
+    // T-shape
+    mShapes[SHAPE_T].append("..X.");
+    mShapes[SHAPE_T].append(".XX.");
+    mShapes[SHAPE_T].append("..X.");
+    mShapes[SHAPE_T].append("....");
+    mColors[SHAPE_T] = sf::Color(0xff0040ff);
+}
+
+bool Game::isValidCoordinates(int x, int y) const
+{
+    return x >= 0 && x < FIELD_WIDTH && y >= 0 && y < FIELD_HEIGHT;
+}
+
+void Game::centralizeWindow()
+{
+    const int screenWidth = sf::VideoMode::getDesktopMode().width;
+    const int screenHeight = sf::VideoMode::getDesktopMode().height;
+    mWindow.setPosition(sf::Vector2i((screenWidth - SCREEN_WIDTH) / 2,
+                                     (screenHeight - SCREEN_HEIGHT) / 2));
 }
--- game.hpp
+++ game.hpp
@@ -5,6 +5,7 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <string>
+#include <map>
 
 class Game
 {
@@ -13,20 +14,57 @@ public:
     void run();
 
 private:
+    // I L J O S Z T
     enum
     {
-        SCREEN_WIDTH = 640,
-        SCREEN_HEIGHT = 480,
-        FPS = 30
+        FIELD_WIDTH = 12,
+        FIELD_HEIGHT = 20,
+        BLOCK_SIZE = 30,
+        SCREEN_WIDTH = FIELD_WIDTH * BLOCK_SIZE,
+        SCREEN_HEIGHT = FIELD_HEIGHT * BLOCK_SIZE,
+        FPS = 30,
+        EMPTY = 0,
+        SHAPE_I = 1,
+        SHAPE_L = 2,
+        SHAPE_J = 3,
+        SHAPE_O = 4,
+        SHAPE_S = 5,
+        SHAPE_Z = 6,
+        SHAPE_T = 7,
+        BOUNDARY_BLOCK = 9,
+        BLOCKS_IN_PIECE = 4,
+        NUM_SHAPES = 7
     };
     const std::string TITLE_OF_MAIN_WINDOW { "Tetris" };
     const sf::Time mFrameTime { sf::seconds(1.0f /FPS) };
     sf::RenderWindow mWindow;
+    std::map<int, std::string> mShapes;
+    std::map<int, sf::Color> mColors;
+    int mField[FIELD_HEIGHT * FIELD_WIDTH];
+
+    struct Tetramino
+    {
+        int type;
+        int x, y;
+        int rotation;
+    };
+
+    Tetramino mCurrentPiece;
+    bool mGameOver;
 
     void inputPhase();
     void updatePhase(sf::Time frameTime);
     void renderPhase();
+    void drawField();
+    void drawCurrentPiece();
     void centralizeWindow();
+    void createAssets();
+    void initField();
+    void spawnNewPiece();
+    bool isValidCoordinates(int x, int y) const;
+    int rotate(int blockX, int blockY, int angle) const;
+
+    bool fit(const Tetramino& tetramino, int destX, int destY, int rotation);
 };
 
 #endif // GAME_HPP
